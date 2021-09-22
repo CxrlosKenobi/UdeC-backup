d151aa5a34279408ce710f0b3b8d75de592ac7f8
CxrlosKenobi <yolitopinto@gmail.com>
2021-09-22 11:04:16 -0300

    Update listado 1 IDSI

--- IDSI/Listado1.py
+++ IDSI/Listado1.py
@@ -1,9 +1,16 @@
+# 1.- ADDITION
 def addition():
-	m = int(input('M = '))
-	n = int(input('N = '))
+	while True:
+		try:
+			m = int(input('M = '))
+			n = int(input('N = '))
 
-	if n >= 0 and m >= 0:
-		print(f'\n{m} - {n} = {m - n}\n')
+			if n >= 0 and m >= 0:
+				print(f'\n{m} - {n} = {m - n}\n')
+		except KeyboardInterrupt:
+			exit(0)
 
-while True:
-	addition()
+# 2.- "Perfect numbers"
+def perfectNumbers():
+	n = int(input('> '))
+	
